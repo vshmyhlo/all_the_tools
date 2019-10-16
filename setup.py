60176c5d62c8fec47d0ed43168d8dd2bea6a7fc5
Vlad Shmyhlo <aion.stu@gmail.com>
2019-10-16 14:04:36 +0300

    Update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='all_the_tools',
+    version='0.0.1',
+    packages=find_packages(),
+    description='All The Tools',
+    author='Vlad Shmyhlo',
+    author_email='aion.stu@gmail.com',
+    url='https://github.com/v-shmyhlo/all_the_tools')
