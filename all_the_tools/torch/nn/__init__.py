69d5d14d29921693493717f12f00c4b4ecc5896b
Vlad Shmyhlo <aion.stu@gmail.com>
2019-11-26 17:50:12 +0200

    Update

---
+++ all_the_tools/torch/nn/__init__.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+
+
+class FilterResponseNorm(nn.Module):
+    def __init__(self, num_features, eps=1e-5):
+        super().__init__()
+
+        self.eps = eps
+        self.weight = nn.Parameter(torch.empty(1, num_features, 1, 1))
+        self.bias = nn.Parameter(torch.empty(1, num_features, 1, 1))
+
+        nn.init.ones_(self.weight)
+        nn.init.zeros_(self.bias)
+
+    def forward(self, input):
+        nu = (input**2).mean((2, 3), keepdim=True)
+        nu = torch.sqrt(nu + self.eps)
+
+        input = input / nu
+        input = input * self.weight + self.bias
+
+        return input
+
+
+class TLU(nn.Module):
+    def __init__(self, num_features):
+        super().__init__()
+
+        self.tau = nn.Parameter(torch.empty(1, num_features, 1, 1))
+
+    def forward(self, input):
+        input = torch.max(input, self.tau)
+
+        return input
---
+++ test/torch/test_nn.py
@@ -0,0 +1,17 @@
+import torch
+
+from all_the_tools.torch.nn import FilterResponseNorm, TLU
+
+
+def test_filter_response_norm():
+    m = FilterResponseNorm(8)
+    input = torch.zeros(4, 8, 10, 10)
+    output = m(input)
+    assert output.size() == (4, 8, 10, 10)
+
+
+def test_tlu():
+    m = TLU(8)
+    input = torch.zeros(4, 8, 10, 10)
+    output = m(input)
+    assert output.size() == (4, 8, 10, 10)
