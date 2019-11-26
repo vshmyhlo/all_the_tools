1a76fc061131efc2e81bdedb26bebbbf1a78cd15
Vlad Shmyhlo <aion.stu@gmail.com>
2019-11-26 17:56:49 +0200

    Update

--- all_the_tools/torch/nn/__init__.py
+++ all_the_tools/torch/nn/__init__.py
@@ -3,19 +3,22 @@ import torch.nn as nn
 
 
 class FilterResponseNorm(nn.Module):
-    def __init__(self, num_features, eps=1e-5):
+    def __init__(self, num_features, eps=1e-6):
         super().__init__()
 
         self.eps = eps
+        self.eps_p = nn.Parameter(torch.empty(1, num_features, 1, 1))
         self.weight = nn.Parameter(torch.empty(1, num_features, 1, 1))
         self.bias = nn.Parameter(torch.empty(1, num_features, 1, 1))
 
+        nn.init.zeros_(self.eps_p)
         nn.init.ones_(self.weight)
         nn.init.zeros_(self.bias)
 
     def forward(self, input):
         nu = (input**2).mean((2, 3), keepdim=True)
-        nu = torch.sqrt(nu + self.eps)
+        eps = self.eps + self.eps_p.abs()
+        nu = torch.sqrt(nu + eps)
 
         input = input / nu
         input = input * self.weight + self.bias
