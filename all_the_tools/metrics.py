e22ff05554a1828d6d8d3c0ff646f4a4387fc884
Vlad Shmyhlo <aion.stu@gmail.com>
2019-12-03 14:38:46 +0200

    Update

--- all_the_tools/metrics.py
+++ all_the_tools/metrics.py
@@ -1,7 +1,24 @@
 import numpy as np
 
 
-class Mean(object):
+class Metric(object):
+    def compute(self):
+        raise NotImplementedError
+
+    def update(self, value):
+        raise NotImplementedError
+
+    def reset(self):
+        raise NotImplementedError
+
+    def compute_and_reset(self):
+        value = self.compute()
+        self.reset()
+
+        return value
+
+
+class Mean(Metric):
     def __init__(self, axis=None):
         self.axis = axis
         self.values = []
@@ -9,15 +26,23 @@ class Mean(object):
     def compute(self):
         values = np.concatenate(self.values, 0)
         return values.mean(self.axis)
-  
+
     def update(self, value):
         self.values.append(value)
 
     def reset(self):
         self.values = []
 
-    def compute_and_reset(self):
-        value = self.compute()
-        self.reset()
 
-        return value
+class Last(Metric):
+    def __init__(self):
+        self.value = None
+
+    def compute(self):
+        return self.value
+
+    def update(self, value):
+        self.value = value
+
+    def reset(self):
+        self.value = None
