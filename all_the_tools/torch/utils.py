4f3d988c0c8a75f6dc7ad97509d13bd0dd99ed58
Vlad Shmyhlo <aion.stu@gmail.com>
2020-01-16 17:32:06 +0200

    Update

--- all_the_tools/torch/utils.py
+++ all_the_tools/torch/utils.py
@@ -31,7 +31,7 @@ def one_hot(input, num_classes, dtype=torch.float):
 
 def seed_torch(seed):
     torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
