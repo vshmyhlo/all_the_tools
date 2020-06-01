4cf0155a306675dc361765ab4ea5968b8871f5ca
Vlad Shmyhlo <aion.stu@gmail.com>
2020-06-01 13:47:42 +0300

    Update

--- all_the_tools/torch/optim/__init__.py
+++ all_the_tools/torch/optim/__init__.py
@@ -36,25 +36,25 @@ class DummySwitchable(torch.optim.Optimizer):
         super().load_state_dict(state_dict['state_dict'])
 
 
-class EWA(torch.optim.Optimizer):
+class EMA(torch.optim.Optimizer):
     def __init__(self, optimizer, momentum, num_steps):
         self.optimizer = optimizer
         self.training = False
 
-        defaults = dict(ewa_momentum=momentum, ewa_num_steps=num_steps, ewa_step_counter=0)
+        defaults = dict(ema_momentum=momentum, ema_num_steps=num_steps, ema_step_counter=0)
         super().__init__(optimizer.param_groups, defaults)
 
-    def update_ewa_group(self, group):
+    def update_ema_group(self, group):
         for p in group['params']:
             param_state = self.state[p]
 
-            if 'ewa_param' not in param_state:
-                param_state['ewa_param'] = torch.empty_like(p.data)
-                param_state['ewa_param'].copy_(p.data)
+            if 'ema_param' not in param_state:
+                param_state['ema_param'] = torch.empty_like(p.data)
+                param_state['ema_param'].copy_(p.data)
 
-            ewa_p = param_state['ewa_param']
-            mom = group['ewa_momentum']
-            ewa_p.mul_(mom).add_(1 - mom, p.data)
+            ema_p = param_state['ema_param']
+            mom = group['ema_momentum']
+            ema_p.mul_(mom).add_(1 - mom, p.data)
 
     def step(self, closure=None):
         assert self.training
@@ -62,11 +62,11 @@ class EWA(torch.optim.Optimizer):
         loss = self.optimizer.step(closure)
 
         for group in self.param_groups:
-            group['ewa_step_counter'] += 1
-            step_counter = group['ewa_step_counter']
+            group['ema_step_counter'] += 1
+            step_counter = group['ema_step_counter']
 
-            if step_counter % group['ewa_num_steps'] == 0:
-                self.update_ewa_group(group)
+            if step_counter % group['ema_num_steps'] == 0:
+                self.update_ema_group(group)
 
         return loss
 
@@ -78,13 +78,13 @@ class EWA(torch.optim.Optimizer):
             for p in group['params']:
                 param_state = self.state[p]
 
-                if 'ewa_saved_param' not in param_state:
-                    param_state['ewa_saved_param'] = torch.empty_like(p.data)
-                    param_state['ewa_saved_param'].copy_(p.data)
+                if 'ema_saved_param' not in param_state:
+                    param_state['ema_saved_param'] = torch.empty_like(p.data)
+                    param_state['ema_saved_param'].copy_(p.data)
 
-                ewa_saved_p = param_state['ewa_saved_param']
+                ema_saved_p = param_state['ema_saved_param']
 
-                p.data.copy_(ewa_saved_p)
+                p.data.copy_(ema_saved_p)
 
     def eval(self):
         assert self.training
@@ -94,11 +94,11 @@ class EWA(torch.optim.Optimizer):
             for p in group['params']:
                 param_state = self.state[p]
 
-                ewa_p = param_state['ewa_param']
-                ewa_saved_p = param_state['ewa_saved_param']
+                ema_p = param_state['ema_param']
+                ema_saved_p = param_state['ema_saved_param']
 
-                ewa_saved_p.copy_(p.data)
-                p.data.copy_(ewa_p)
+                ema_saved_p.copy_(p.data)
+                p.data.copy_(ema_p)
 
     def state_dict(self):
         return {
